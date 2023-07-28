6b74fde1cf853f57690d0efef5cc1a0fea033ddb
david <vekkerdavid1235@gmail.com>
2023-07-28 14:20:22 +0400

    Update: Describe the changes you made here

---
+++ static/JS/java.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const backToTopButton = document.querySelector(".back-to-top");
+    backToTopButton.addEventListener("click", function() {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+  });
